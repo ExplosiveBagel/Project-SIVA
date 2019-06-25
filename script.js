831a34aef89ebe2294e11ca54377de9002aced0b
Kieran Mackenzie <51792170+ExplosiveBagel@users.noreply.github.com>
2019-06-25 18:04:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
 }
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    document.GetElementById('PhoneNav').style.display: block;
+  } else {
+    document.GetElementById('PhoneNav').style.display: none;
+  }
+}
+
+var x = window.matchMedia("(max-width: 1023px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
